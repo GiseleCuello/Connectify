b6fdee269438c9470a498223d386c0109da7fef4
Martin Lamacchia <martinjiro22@gmail.com>
2023-10-13 16:47:11 -0300

    Iniciando el BACK - Conexion a la DB

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const server = require("./src/app");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB;
+
+// Conecta a MongoDB primero
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+
+    // Luego inicia el servidor
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connection", error);
+  });
