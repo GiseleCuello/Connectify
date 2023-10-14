7f744aafa5c4463268ba8273aad8b04d856b75ec
Andy Ojeda <tuar_a@hotmail.com>
2023-10-14 11:11:39 -0300

    Probando

--- server/src/models/Admin.js
+++ server/src/models/Admin.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+console.log("PROBANDO ANDO...!!")
 const AdminSchema = mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
