3afa0eea4e530009ff902c351872cf42a54d2118
Andy Ojeda <tuar_a@hotmail.com>
2023-10-18 13:47:29 -0300

    clientLogin funcionando OK

---
+++ server/src/controllers/Client/clientLogin.js
@@ -0,0 +1,28 @@
+const Client = require("../../models/Client");
+
+const bcryptjs = require('bcryptjs');
+
+const clientLogin = async (req, res) =>{
+    const {email, password} = req.body;
+
+    try {
+        const clientSearch = await Client.findOne({email:email})
+        
+        if (!clientSearch){
+            return res.status(404).json({message:"Usuario no encontrado"});
+        }
+
+        const passIsMatch = await bcryptjs.compare(password,clientSearch.password);
+
+        if(!passIsMatch){
+            return res.status(400).json({message:"Password Incorrecto"});
+        }
+
+        res.status(200).json(clientSearch);
+
+    } catch (error) {
+        res.status(500).json({error:"Error en clientLogin...", error})
+    }
+}
+
+module.exports = clientLogin;
