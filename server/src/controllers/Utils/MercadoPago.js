5e1ef6b516146de11c6fbd2d72f4f7559adcbb39
Martin Lamacchia <martinjiro22@gmail.com>
2023-10-21 09:46:22 -0300

    Descomentado mercadoPago e incluyendo variables de entorno de MP

--- server/src/controllers/Utils/MercadoPago.js
+++ server/src/controllers/Utils/MercadoPago.js
@@ -1,44 +1,43 @@
-// require('dotenv').config();
+require("dotenv").config();
 
-// const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-// const MERCADOPAGO_SUCCESS = process.env.MERCADOPAGO_SUCCESS;
-// const MERCADOPAGO_FAILURE = process.env.MERCADOPAGO_FAILURE;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const MERCADOPAGO_SUCCESS = process.env.MERCADOPAGO_SUCCESS;
+const MERCADOPAGO_FAILURE = process.env.MERCADOPAGO_FAILURE;
 
-// const mercadopago = require("mercadopago");
+const mercadopago = require("mercadopago");
 
-// mercadopago.configure({
-// 	access_token: ACCESS_TOKEN,
-// });
+mercadopago.configure({
+  access_token: ACCESS_TOKEN,
+});
 
-// const mercadoPago = (req, res) => {
-//   // Datos que vienen por body desde el FRONT.
-//   let preference = {
-//     items: [
-//       {
-//         title: req.body.description,
-//         unit_price: Number(req.body.price),
-//         quantity: Number(req.body.quantity),
-//       }
-//     ],
-//     back_urls: {
-//       "success": MERCADOPAGO_SUCCESS,
-//       "failure": MERCADOPAGO_FAILURE,
-//       "pending": ""
-//     },
-//     auto_return: "approved",
-//   };
+const mercadoPago = (req, res) => {
+  // Datos que vienen por body desde el FRONT.
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity),
+      },
+    ],
+    back_urls: {
+      success: MERCADOPAGO_SUCCESS,
+      failure: MERCADOPAGO_FAILURE,
+      pending: "",
+    },
+    auto_return: "approved",
+  };
 
-//   mercadopago.preferences
-//     .create(preference)
-//     .then(function (response) {
-//       res.json({
-//         id: response.body.id    //! Mando el id del pago al FRONT
-//       });
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     });
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id, //! Mando el id del pago al FRONT
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
-// };
-
-// module.exports = mercadoPago;
+module.exports = mercadoPago;
