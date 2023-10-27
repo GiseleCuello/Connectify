d6c060dd5d5dc1084f8e7d95f1e65ffc78f77545
GGuenov <german.guenov@gmail.com>
2023-10-27 20:30:00 +0100

    Debug delete and Set it as Reversible

--- server/src/controllers/Professional/ProfessionalDelete.js
+++ server/src/controllers/Professional/ProfessionalDelete.js
@@ -4,20 +4,53 @@ const professionalDelete = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const professionalUpdate = await Professional.findByIdAndUpdate(
-      { _id: id },
-      req.body,
-      { new: true }
-    );
+    // Find the professional document by its ID
+    const existingProfessional = await Professional.findById(id);
 
-    if (!professionalUpdate) {
+    if (!existingProfessional) {
       return res.status(400).json({ message: "No se encontró al usuario" });
     }
 
+    // Toggle the value of 'isDeleted'
+    const isDeleted = !existingProfessional.isDeleted;
+
+    // Use $set to update the 'isDeleted' field
+    const professionalUpdate = await Professional.findByIdAndUpdate(
+      { _id: id },
+      { $set: { isDeleted } },
+      { new: true }
+    );
+
     res.status(200).json(professionalUpdate);
   } catch (error) {
-    res.status(500).json({ error: "Error del servido", error });
+    res.status(500).json({ error: "Error del servidor", error });
   }
 };
 
 module.exports = professionalDelete;
+
+// const Professional = require("../../models/Professional");
+
+// const professionalDelete = async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     req.body.isDeleted = false;
+
+//     const professionalUpdate = await Professional.findByIdAndUpdate(
+//       { _id: id },
+//       req.body,
+//       { new: true }
+//     );
+
+//     if (!professionalUpdate) {
+//       return res.status(400).json({ message: "No se encontró al usuario" });
+//     }
+
+//     res.status(200).json(professionalUpdate);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error del servido", error });
+//   }
+// };
+
+// module.exports = professionalDelete;
