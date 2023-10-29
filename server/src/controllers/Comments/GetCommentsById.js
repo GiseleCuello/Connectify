92fd01ed82bde696744901f8a136f61fd53a0116
giselecuello <giselecuello@gmail.com>
2023-10-29 16:07:13 -0300

    arreglando comentarios

--- server/src/controllers/Comments/GetCommentsById.js
+++ server/src/controllers/Comments/GetCommentsById.js
@@ -2,18 +2,18 @@ const Comment = require("../../models/Comment");
 
 const getById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const comment = await Comment.findById(id)
+    const { professionalId } = req.params;
+    const comment = await Comment.find({ professional: professionalId })
       .populate("client") 
       .populate("professional") 
       .exec();
 
-    if (!comment) {
-      return res.status(404).json({ error: "Comment not found." });
+      if (!comment || comment.length === 0) {
+      return res.status(404).json({ error: "Comment not found for this professional." });
     }
     res.status(200).json(ad);
   } catch (error) {
-    res.status(500).json({ error: "Error getting comment." });
+    res.status(500).json({ error: "Error getting comments by professional id." });
   }
 };
 
