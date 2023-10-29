12062f46954996340bbdc68e78cf2b2f4c792cfd
GGuenov <german.guenov@gmail.com>
2023-10-29 21:16:48 +0000

    Register Recuperation whit Cloudinary

--- server/src/controllers/Client/clientRegister.js
+++ server/src/controllers/Client/clientRegister.js
@@ -1,33 +1,42 @@
-const uploadImage = require("../Utils/Cloudinary");
 const Client = require("../../models/Client");
 const Professional = require("../../models/Professional");
+const uploadImage = require("../Utils/Cloudinary");
 
 const clientRegister = async (req, res) => {
   try {
-    const { name, lastName, userName, email, image, address, password } = req.body;
+    const { name, lastName, userName, email, province, location, password } =
+      req.body;
 
-    // Busca si ya existe un usuario registrado con ese correo o userName
-    const existingUser = await Client.findOne({ $or: [{ email }, { userName }] });
+    const result = await uploadImage(req.files.image.tempFilePath);
+    //Busco usuario ya registrado con ese nombre...
+    const checkProf = await Professional.findOne({
+      $or: [{ email: email }, { userName: userName }],
+    });
+    const checkClient = await Client.findOne({
+      $or: [{ email: email }, { userName: userName }],
+    });
 
-    if (existingUser) {
+    if (checkProf || checkClient) {
       return res.status(400).json({ message: "Usuario ya registrado" });
     }
 
-    // Crea una nueva instancia de Cliente
+    // Creo una nueva instancia de Cliente...
     const newClient = new Client({
       name,
       lastName,
       userName,
       email,
-      image,
+      image: result.secure_url,
       password,
-      address,
+      province,
+      location,
     });
 
     await newClient.save();
+
     res.status(201).json({ message: "Successfully registered client." });
   } catch (error) {
-    res.status(500).json({ error: "Error registering client...!", error });
+    res.status(501).json({ error: "Error registering client...!", error });
   }
 };
 
