9caea9cc02b0173b706940c4bc7c3f34568bcd0f
Martin Lamacchia <martinjiro22@gmail.com>
2023-10-31 17:17:03 -0300

    Login por Body

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,40 +1,36 @@
-const router = require('express').Router();
-const adminRoute = require('./adminRoute');
-const clientRoute = require('./clientRoute');
-const professionalRoute = require('./professionalRoute');
-const commentsRoute = require('./commentsRouter');
-const adsRoute = require('./adsRoute');
-const chatRoute = require('./chatRoute');
-const mercadoPago = require('../controllers/Utils/MercadoPago');
-const postSocket = require('../controllers/Socket/Socketio');
-const favoritesRoute = require('./favoritesRoute');
-const  payment  = require('mercadopago');
-const paymentsRoute = require('./paymentsRoute');
+const router = require("express").Router();
+const adminRoute = require("./adminRoute");
+const clientRoute = require("./clientRoute");
+const professionalRoute = require("./professionalRoute");
+const commentsRoute = require("./commentsRouter");
+const adsRoute = require("./adsRoute");
+const chatRoute = require("./chatRoute");
+const mercadoPago = require("../controllers/Utils/MercadoPago");
+const postSocket = require("../controllers/Socket/Socketio");
+const favoritesRoute = require("./favoritesRoute");
+const payment = require("mercadopago");
+const paymentsRoute = require("./paymentsRoute");
 
+router.use("/client", clientRoute);
 
-router.use('/client', clientRoute);
+router.use("/chat", chatRoute);
 
+router.use("/professional", professionalRoute);
 
-router.use('/chat', chatRoute);
+router.use("/admin", adminRoute);
 
-router.use('/professional', professionalRoute);
+router.use("/comments", commentsRoute);
 
-router.use('/admin', adminRoute);
+router.use("/ads", adsRoute);
 
-router.use('/comments', commentsRoute);
+router.use("/fav", favoritesRoute);
 
-router.use('/ads', adsRoute);
+router.post("/create_preference", mercadoPago);
 
-router.use('/fav', favoritesRoute);
+router.post("/purchase", postSocket);
 
-router.post('/create_preference', mercadoPago);
-
-router.post('/purchase', postSocket);
-
-router.get('/purchase', postSocket);
-
+router.get("/purchase", postSocket);
 
 router.use("/payments", paymentsRoute);
-
 
 module.exports = router;
