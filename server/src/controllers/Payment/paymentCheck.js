090bf647e86efdaa35e1482f0eeef7b044e77238
Martin Lamacchia <martinjiro22@gmail.com>
2023-11-07 19:53:38 -0300

    Cambio - Despues de contratar MP te lleva a HOME

--- server/src/controllers/Payment/paymentCheck.js
+++ server/src/controllers/Payment/paymentCheck.js
@@ -4,19 +4,21 @@ const paymentCheck = async (req, res) => {
   try {
     const { paymentID } = req.params;
 
-    
+    console.log(paymentID);
 
-    const existingPayment = await Payments.findOne({paymentID})
-     
+    const existingPayment = await Payments.findOne({ paymentID });
+
+    console.log(existingPayment);
+
     if (existingPayment) {
-        // Si existe un registro con el mismo paymentID, devuelve un mensaje de éxito.
-        res.status(200).json({ exists: true });
+      // Si existe un registro con el mismo paymentID, devuelve un mensaje de éxito.
+      res.status(200).json({ exists: true });
     } else {
-        // Si no existe un registro con el mismo paymentID, devuelve un mensaje indicando que no existe.
-        res.status(200).json({ exists: false });
+      // Si no existe un registro con el mismo paymentID, devuelve un mensaje indicando que no existe.
+      res.status(200).json({ exists: false });
     }
   } catch (error) {
-    res.status(500).json({ error: error.message});
+    res.status(500).json({ error: error.message });
   }
 };
 
--- server/src/controllers/Utils/MercadoPago.js
+++ server/src/controllers/Utils/MercadoPago.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const MERCADOPAGO_SUCCESS = process.env.MERCADOPAGO_SUCCESS;
@@ -7,46 +7,43 @@ const MERCADOPAGO_FAILURE = process.env.MERCADOPAGO_FAILURE;
 const mercadopago = require("mercadopago");
 
 mercadopago.configure({
-access_token: ACCESS_TOKEN,
+  access_token: ACCESS_TOKEN,
 });
 
 const mercadoPago = (req, res) => {
+  // const SUCCESS = `http://localhost:5173/payments/${req.body.userName}?idProf=${req.body.idProf}`;
+  const SUCCESS = `${MERCADOPAGO_SUCCESS}/home`
+  // const FAILURE = `http://localhost:5173/payments/${req.body.userName}?idProf=${req.body.idProf}`;
+  const FAILURE = `${MERCADOPAGO_FAILURE}/home`
 
-    // const SUCCESS = `http://localhost:5173/payments/${req.body.userName}?idProf=${req.body.idProf}`
-    const SUCCESS = `${MERCADOPAGO_SUCCESS}/payments/${req.body.userName}?idProf=${req.body.idProf}`
-    // const FAILURE = `http://localhost:5173/payments/${req.body.userName}?idProf=${req.body.idProf}`
-    const FAILURE = `${MERCADOPAGO_FAILURE}/payments/${req.body.userName}?idProf=${req.body.idProf}`
+  // console.log("utilsMP!!!", req.body);
 
-// console.log("utilsMP!!!", req.body);
-
-    let preference = {
-        items: [
-          {
-            title: req.body.description,
-            unit_price: Number(req.body.price),
-            quantity: Number(req.body.quantity),
-            
-          }
-        ],
-        back_urls: {
-          "success": SUCCESS,     //MERCADOPAGO_SUCCESS,
-          "failure": FAILURE,     //MERCADOPAGO_FAILURE,
-          "pending": ""
-        },
-        auto_return: "approved",
-    };
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity),
+      },
+    ],
+    back_urls: {
+      success: SUCCESS, //MERCADOPAGO_SUCCESS,
+      failure: FAILURE, //MERCADOPAGO_FAILURE,
+      pending: "",
+    },
+    auto_return: "approved",
+  };
 
   mercadopago.preferences
     .create(preference)
     .then(function (response) {
       res.json({
-        id: response.body.id    // Mando el id del pago al FRONT
+        id: response.body.id, // Mando el id del pago al FRONT
       });
     })
     .catch(function (error) {
       console.log(error);
     });
-
 };
 
 module.exports = mercadoPago;
