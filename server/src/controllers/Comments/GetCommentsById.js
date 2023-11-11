c6708c3e2834327dd4ea691ddf02beee9c5f39d5
GGuenov <german.guenov@gmail.com>
2023-11-11 13:09:56 +0000

    Comment By Id Debugged

--- server/src/controllers/Comments/GetCommentsById.js
+++ server/src/controllers/Comments/GetCommentsById.js
@@ -1,17 +1,18 @@
 const Comment = require("../../models/Comment");
 
 const getById = async (req, res) => {
+  const { id } = req.params;
   try {
-    const { id } = req.params;
+    console.log(id);
     const comment = await Comment.findById(id)
-      .populate("client") 
-      .populate("professional") 
+      .populate("Client")
+      .populate("Professional")
       .exec();
 
     if (!comment) {
       return res.status(404).json({ error: "Comment not found." });
     }
-    res.status(200).json(ad);
+    res.status(200).json(comment);
   } catch (error) {
     res.status(500).json({ error: "Error getting comment." });
   }
