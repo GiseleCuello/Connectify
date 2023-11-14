58f6a1d8a4bc7bf982d891d06cffd630243bbb0c
Drexleer <drexleer01@gmail.com>
2023-11-14 00:53:18 -0500

    Ya no se puede crear mas de 2 anuncios

--- server/src/controllers/Ads/createAd.js
+++ server/src/controllers/Ads/createAd.js
@@ -3,12 +3,20 @@ const NewAd = require('../../models/NewAd');
 // Controlador para crear un anuncio
 const createAd = async (req, res) => {
   try {
-    const createAd = req.body;
-    const newAdData = await NewAd.create(createAd);
-    // .populate("creator") // Esto poblará los datos del profesional
-    //   .exec(); //ejecuta la consulta y obtiene resultados
-    console.log(newAdData);
-    res.status(201).json(newAdData);
+    const creatorId = req.body.creator;
+    // Verificar la cantidad de anuncios existentes para el creador
+    const existingAdsCount = await NewAd.countDocuments({ creator: creatorId });
+
+    // Permitir la creación si el número de anuncios existentes es menor que 2
+    if (existingAdsCount < 2) {
+      const newAdData = await NewAd.create(req.body);
+      console.log(newAdData);
+      res.status(201).json(newAdData);
+    } else {
+      res
+        .status(400)
+        .json({ error: 'No se pueden crear más de dos anuncios.' });
+    }
   } catch (error) {
     res.status(500).json({ error: 'Error creating ad.', error });
   }
