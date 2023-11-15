5762bc4d04dd412a3c4435f785fe8b6efd4e3054
GGuenov <german.guenov@gmail.com>
2023-11-15 13:14:56 +0000

    ..

--- server/src/router/paymentsRoute.js
+++ server/src/router/paymentsRoute.js
@@ -3,12 +3,12 @@ const paymentsRegister = require("../controllers/Payment/paymentsRegister");
 const paymentsByUser = require("../controllers/Payment/paymentsByUser");
 const paymentsGet = require("../controllers/Payment/paymentGet");
 const paymentCheck = require("../controllers/Payment/paymentCheck");
+const paymentsToProfs = require("../controllers/Payment/paymentsToProfs");
 
-paymentsRoute.post("/register", paymentsRegister); 
-paymentsRoute.get("/search/:userName", paymentsByUser); 
-paymentsRoute.get("/", paymentsGet); 
-paymentsRoute.get("/check/:paymentID", paymentCheck); 
-
+paymentsRoute.post("/register", paymentsRegister);
+paymentsRoute.get("/search/:userName", paymentsByUser);
+paymentsRoute.get("/search/professionals/:_id", paymentsToProfs);
+paymentsRoute.get("/", paymentsGet);
+paymentsRoute.get("/check/:paymentID", paymentCheck);
 
 module.exports = paymentsRoute;
-
