275a9900224f9a102a4ba4a7dbcaa376afe29a96
Drexleer <drexleer01@gmail.com>
2023-11-14 22:51:34 -0500

    Arreglado backend para que filtre los anuncios borrados

--- server/src/controllers/Ads/getAllAds.js
+++ server/src/controllers/Ads/getAllAds.js
@@ -2,9 +2,9 @@ const NewAd = require('../../models/NewAd');
 
 const getAllAds = async (req, res) => {
   try {
-    const ads = await NewAd.find()
-    .populate("creator") // Esto poblará los datos del profesional
-      .exec(); //ejecuta la consulta y obtiene resultados
+    const ads = await NewAd.find({ isDeleted: false })
+      .populate('creator')
+      .exec();
     res.status(200).json(ads);
   } catch (error) {
     res.status(500).json({ error: 'Error creating ad.' });
