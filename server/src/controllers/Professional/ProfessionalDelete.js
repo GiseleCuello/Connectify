5e8630c0e8b8d2feb483afc178adf56db88c6371
GGuenov <german.guenov@gmail.com>
2023-11-16 02:32:02 +0000

    Fail To Log When Banned

--- server/src/controllers/Professional/ProfessionalDelete.js
+++ server/src/controllers/Professional/ProfessionalDelete.js
@@ -2,19 +2,32 @@ const Professional = require("../../models/Professional");
 
 const professionalDelete = async (req, res) => {
   const { id } = req.params;
-
   try {
     const existingProfessional = await Professional.findById(id);
 
     if (!existingProfessional) {
       return res.status(400).json({ message: "No se encontró al profesional" });
     }
-
+    let types = "";
     const isDeleted = !existingProfessional.isDeleted;
-
+    if (existingProfessional.types === "professional") {
+      types = "professionat";
+    } else {
+      if (existingProfessional.types === "professionat") {
+        types = "professional";
+      } else {
+        if (existingProfessional.types === "client") {
+          types = "clienl";
+        } else {
+          if (existingProfessional.types === "clienl") {
+            types = "client";
+          }
+        }
+      }
+    }
     const professionalUpdate = await Professional.findByIdAndUpdate(
       { _id: id },
-      { $set: { isDeleted } },
+      { $set: { isDeleted, types: types } },
       { new: true }
     );
 
